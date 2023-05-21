86b2062f9e8fa5b8a2c38e13eaeb83ac42ae013b
SanthiyaPeriyasamy245 <113085174+SanthiyaPeriyasamy245@users.noreply.github.com>
2023-05-21 23:33:02 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,22 @@
+class basic
+{
+    public static void main(String a[])
+    {
+       System.out.println("Welcome to java!");
+       int n=15;
+       byte r=127; // rnage of -2^7 to 2^7.
+       float f=9.9f;
+       double d=9.9;
+       long l=123456l;
+       short s=234;
+       boolean b=false;
+       System.out.println(n);
+       System.out.println(r);
+       System.out.println(f);
+       System.out.println(d);
+       System.out.println(l);
+       System.out.println(s);
+       System.out.println(b);
+
+    }
+}
