956cd266b08f5d7008cdce9a10e169176610522f
SanthiyaPeriyasamy245 <113085174+SanthiyaPeriyasamy245@users.noreply.github.com>
2023-06-07 23:43:24 +0530

    Add files via upload

---
+++ basic5.java
@@ -0,0 +1,19 @@
+public class basic5 {
+    public static void main(String a[])
+    {
+
+        String s="Sivakumar";
+        String s1=new String("Santhiya");
+        
+        System.out.println(s1.hashCode());
+        System.out.println(s1.charAt(1));
+        System.out.println(s1.concat("Periyasamy"));
+        System.out.println(s1.indexOf('y'));
+       
+
+        
+
+
+    }
+    
+}
