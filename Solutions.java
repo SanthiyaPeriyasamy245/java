183e6bd67a4b617581558231ddc11317f5816bb5
SanthiyaPeriyasamy245 <113085174+SanthiyaPeriyasamy245@users.noreply.github.com>
2023-08-28 00:36:59 +0530

    Add files via upload

---
+++ Solutions.java
@@ -0,0 +1,35 @@
+
+public class Solutions {
+  public static void main(String[]args)
+  {
+      int n=1345236469;
+    System.out.println( reverse(n));
+  }
+  public static  int reverse(int x) {
+    if (x == 0) {
+        return 0;
+    }
+    long rev = 0;
+    while (x != 0) {
+        rev = rev * 10 + x % 10;
+        x = x / 10;
+    }
+    if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
+        return 0;
+    }
+    return (int) rev;
+}
+  
+}
+  
+ 
+  
+
+
+  
+ 
+  
+
+  
+
+  
