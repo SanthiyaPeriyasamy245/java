9eb808111ed79206efb268440a58d8fd01749e9c
SanthiyaPeriyasamy245 <113085174+SanthiyaPeriyasamy245@users.noreply.github.com>
2023-08-27 08:48:45 +0530

    Add files via upload

---
+++ PascalSum.java
@@ -0,0 +1,17 @@
+// nth row sum.
+public class PascalSum{
+  public  static void main(String[]args)
+  {
+    /*
+    concept each row sum=2^n-1;
+    1 
+    1 1 
+    1 2 1
+    1 4 6 4 1
+    1 5 10 10 5 1
+    */
+    int n=5;
+    System.out.println((int)Math.pow(2,n-1));
+     System.out.println(1<<(n-1));
+  }
+}
